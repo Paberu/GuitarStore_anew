639a023567887517c633364c1062a0ca54f331bd
Paberu <paberu.tocador@gmail.com>
2022-11-21 06:44:18 +0300

    Done. Need more tests and getting it to the Internet.

--- blog/tests/test_comment.py
+++ blog/tests/test_comment.py
@@ -8,39 +8,43 @@ class CommentModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         post = Post.objects.create(title='Main', subtitle='Submain', slug='slug', author='Клим Чугункин',
-                           email='klim@chugun.ru', short_text='Укороченная версия поста',
-                           full_text='Увеличенная версия поста, чтоб прям на две страницы, \
+                                   email='klim@chugun.ru', short_text='Укороченная версия поста',
+                                   full_text='Увеличенная версия поста, чтоб прям на две страницы, \
                            со всяким говном и палками')
-        comment = Comment.objects.create(author='Клим Чугункин', email='klim@chugun.ru',
-                                         text='Укороченная версия поста!', post=post)
-
-    # def setUp(self) -> None:
-    #     print('setUp вызывается перед каждым тестом')
-    #
-    # def tearDown(self) -> None:
-    #     print('tearDown вызывается после каждого теста')
+        Comment.objects.create(author='Клим Чугункин', email='klim@chugun.ru',
+                               text='Укороченная версия поста!', post=post)
 
     def test_author_max_length(self):
         comment = Comment.objects.get(id=1)
         max_length = comment._meta.get_field('author').max_length
         self.assertEqual(max_length, 100)
 
+    def test_author_verbose(self):
+        comment = Comment.objects.get(id=1)
+        verbose = comment._meta.get_field('author').verbose_name
+        self.assertEqual(verbose, 'Автор')
+
     def test_email_max_length(self):
         comment = Comment.objects.get(id=1)
         max_length = comment._meta.get_field('email').max_length
         self.assertEqual(max_length, 100)
+
+    def test_email_verbose(self):
+        comment = Comment.objects.get(id=1)
+        verbose = comment._meta.get_field('email').verbose_name
+        self.assertEqual(verbose, 'E-mail')
 
     def test_text_max_length(self):
         comment = Comment.objects.get(id=1)
         max_length = comment._meta.get_field('text').max_length
         self.assertEqual(max_length, 200)
 
+    def test_text_verbose(self):
+        comment = Comment.objects.get(id=1)
+        verbose = comment._meta.get_field('text').verbose_name
+        self.assertEqual(verbose, 'Содержимое комментария')
+
     def test_parent_post(self):
         comment = Comment.objects.get(id=1)
         post_id = comment.post.id
         self.assertEqual(post_id, 1)
-
-    def test_subtitle_blank(self):
-        post = Post.objects.get(id=1)
-        blank = post._meta.get_field('subtitle').blank
-        self.assertTrue(blank)
--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -8,13 +8,13 @@ class BlogViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         post1 = Post.objects.create(title='Main', subtitle='Submain', slug='slug', author='Клим Чугункин',
-                           email='klim@chugun.ru', short_text='Укороченная версия поста',
-                           full_text='Увеличенная версия поста, чтоб прям на две страницы, \
+                                    email='klim@chugun.ru', short_text='Укороченная версия поста',
+                                    full_text='Увеличенная версия поста, чтоб прям на две страницы, \
                            со всяким говном и палками')
 
         post2 = Post.objects.create(title='Main2', subtitle='Submain2', slug='slug2', author='Клим Чугункин',
-                            email='klim@chugun.ru', short_text='Укороченная версия поста 2',
-                            full_text='Увеличенная версия поста 2, чтоб прям на две страницы, \
+                                    email='klim@chugun.ru', short_text='Укороченная версия поста 2',
+                                    full_text='Увеличенная версия поста 2, чтоб прям на две страницы, \
                            со всяким говном и палками, AAAAA!')
 
         Comment.objects.create(author='Вася', email='vasya@v.ru', text='ЫЫЫЫ!', post=post1)
@@ -25,7 +25,7 @@ class BlogViewTest(TestCase):
         Comment.objects.create(author='Вася', email='vasya@v.ru', text='УУУУУ!', post=post2)
 
         for i in range(8):
-            Photo.objects.create(title='title'+str(i), image='images/3s37mxl39462khqwj0jzf2g366mtmb8i_alvFJcD.webp',
+            Photo.objects.create(title='title' + str(i), image='images/3s37mxl39462khqwj0jzf2g366mtmb8i_alvFJcD.webp',
                                  description='')
 
     def test_200(self):
@@ -47,13 +47,13 @@ class GalleryViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         post1 = Post.objects.create(title='Main', subtitle='Submain', slug='slug', author='Клим Чугункин',
-                           email='klim@chugun.ru', short_text='Укороченная версия поста',
-                           full_text='Увеличенная версия поста, чтоб прям на две страницы, \
+                                    email='klim@chugun.ru', short_text='Укороченная версия поста',
+                                    full_text='Увеличенная версия поста, чтоб прям на две страницы, \
                            со всяким говном и палками')
 
         post2 = Post.objects.create(title='Main2', subtitle='Submain2', slug='slug2', author='Клим Чугункин',
-                            email='klim@chugun.ru', short_text='Укороченная версия поста 2',
-                            full_text='Увеличенная версия поста 2, чтоб прям на две страницы, \
+                                    email='klim@chugun.ru', short_text='Укороченная версия поста 2',
+                                    full_text='Увеличенная версия поста 2, чтоб прям на две страницы, \
                            со всяким говном и палками, AAAAA!')
 
         Comment.objects.create(author='Вася', email='vasya@v.ru', text='ЫЫЫЫ!', post=post1)
@@ -64,20 +64,20 @@ class GalleryViewTest(TestCase):
         Comment.objects.create(author='Вася', email='vasya@v.ru', text='УУУУУ!', post=post2)
 
         for i in range(16):
-            Photo.objects.create(title='title'+str(i), image='images/3s37mxl39462khqwj0jzf2g366mtmb8i_alvFJcD.webp',
+            Photo.objects.create(title='title' + str(i), image='images/3s37mxl39462khqwj0jzf2g366mtmb8i_alvFJcD.webp',
                                  description='')
 
-        def test_200(self):
-            responce = self.client.get('/blog/gallery/')
-            self.assertEqual(responce.status_code, 200)
+    def test_200(self):
+        responce = self.client.get('/blog/gallery')
+        self.assertEqual(responce.status_code, 200)
 
-        def test_template(self):
-            responce = self.client.get('/blog/gallery/')
-            self.assertTemplateUsed(responce, 'gallery.html')
+    def test_template(self):
+        responce = self.client.get('/blog/gallery')
+        self.assertTemplateUsed(responce, 'gallery.html')
 
-        def test_context(self):
-            responce = self.client.get('/blog/gallery/')
-            self.assertEqual(len(responce.context['random_small_images']), 6)
-            self.assertEqual(len(responce.context['last_posts']), 2)
-            self.assertEqual(len(responce.context['last_comments']), 3)
-            self.assertEqual(len(responce.context['photos']), 12)
+    def test_context(self):
+        responce = self.client.get('/blog/gallery')
+        self.assertEqual(len(responce.context['random_small_images']), 6)
+        self.assertEqual(len(responce.context['last_posts']), 2)
+        self.assertEqual(len(responce.context['last_comments']), 3)
+        self.assertEqual(len(responce.context['photos']), 12)
